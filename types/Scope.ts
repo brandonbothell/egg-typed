346d13c76ccd44f8c576000f2de81d08de29e1c9
jasonhaxstuff <shadowmanguyyt@gmail.com>
2018-07-04 11:26:44 -0400

    add jsdocs to scope type

--- types/Scope.ts
+++ types/Scope.ts
@@ -11,7 +11,16 @@ export default interface Scope {
    * The value of a scope's print function.
    */
   print: Function,
+  /**
+   * The value of a scope's array function.
+   */
   array: Function,
+  /**
+   * The value of a scope's sizeof function.
+   */
   sizeof: Function,
+  /**
+   * The value of a scope's element function.
+   */
   element: Function
 }
