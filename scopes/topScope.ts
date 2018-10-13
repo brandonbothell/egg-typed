c38cacb62bd8a14b1c50d9b4954a0e5df76bac1e
Jason Bothel <shadowmanguyyt@gmail.com>
2018-10-13 19:59:09 -0400

    Add more methods and change the example.

--- scopes/topScope.ts
+++ scopes/topScope.ts
@@ -1,5 +1,5 @@
-import { get as http, request as httpReq, RequestOptions } from 'http'
-import { get as https, request as httpsReq } from 'https'
+import { request as httpReq, RequestOptions, IncomingMessage } from 'http'
+import { request as httpsReq } from 'https'
 import { parse as parseUrl } from 'url'
 
 export const topScope = Object.create(null)
@@ -36,31 +36,50 @@ topScope.Array = {
 
 topScope.HTTP = {
   get: (url: string, callback: Function): any => {
-    http(url, (res) => {
-      let data = ''
+    const options = parseUrlToOptions(url, false, 'GET')
+    const req = request(options, false, callback)
 
-      res.on('data', (chunk) => {
-        data += chunk
-      })
-
-      res.on('end', () => {
-        return callback(data)
-      })
+    req.on('error', error => {
+      throw error
     })
+
+    req.end()
   },
   post: (url: string, data: any, callback: Function) => {
-    const options = parseUrlToOptions(url, false)
-    const req = httpReq(options, res => {
-      let data = ''
+    const options = parseUrlToOptions(url, false, 'POST')
+    const req = request(options, false, callback)
 
-      res.setEncoding('utf8')
-      res.on('data', chunk => {
-        data += chunk
-      })
-      res.on('end', () => {
-        return callback(data)
-      })
+    req.on('error', error => {
+      throw error
     })
+
+    req.write(data)
+    req.end()
+  },
+  put: (url: string, data: any, callback: Function) => {
+    const options = parseUrlToOptions(url, true, 'PUT')
+    const req = request(options, false, callback)
+
+    req.on('error', error => {
+      throw error
+    })
+
+    req.write(data)
+    req.end()
+  },
+  delete: (url: string, callback: Function) => {
+    const options = parseUrlToOptions(url, true, 'POST')
+    const req = request(options, false, callback)
+
+    req.on('error', error => {
+      throw error
+    })
+
+    req.end()
+  },
+  patch: (url: string, data: any, callback: Function) => {
+    const options = parseUrlToOptions(url, true, 'PATCH')
+    const req = request(options, true, callback)
 
     req.on('error', error => {
       throw error
@@ -73,31 +92,50 @@ topScope.HTTP = {
 
 topScope.HTTPS = {
   get: (url: string, callback: Function): any => {
-    https(url, (res) => {
-      let data = ''
+    const options = parseUrlToOptions(url, true, 'GET')
+    const req = request(options, true, callback)
 
-      res.on('data', (chunk) => {
-        data += chunk
-      })
-
-      res.on('end', () => {
-        return callback(data)
-      })
+    req.on('error', error => {
+      throw error
     })
+
+    req.end()
   },
   post: (url: string, data: any, callback: Function) => {
-    const options = parseUrlToOptions(url, true)
-    const req = httpsReq(options, res => {
-      let data = ''
+    const options = parseUrlToOptions(url, true, 'POST')
+    const req = request(options, true, callback)
 
-      res.setEncoding('utf8')
-      res.on('data', chunk => {
-        data += chunk
-      })
-      res.on('end', () => {
-        return callback(data)
-      })
+    req.on('error', error => {
+      throw error
     })
+
+    req.write(data)
+    req.end()
+  },
+  put: (url: string, data: any, callback: Function) => {
+    const options = parseUrlToOptions(url, true, 'PUT')
+    const req = request(options, true, callback)
+
+    req.on('error', error => {
+      throw error
+    })
+
+    req.write(data)
+    req.end()
+  },
+  delete: (url: string, callback: Function) => {
+    const options = parseUrlToOptions(url, true, 'POST')
+    const req = request(options, true, callback)
+
+    req.on('error', error => {
+      throw error
+    })
+
+    req.end()
+  },
+  patch: (url: string, data: any, callback: Function) => {
+    const options = parseUrlToOptions(url, true, 'PATCH')
+    const req = request(options, true, callback)
 
     req.on('error', error => {
       throw error
@@ -121,16 +159,38 @@ topScope.JSON = {
   }
 }
 
-function parseUrlToOptions (url: string, https: boolean): RequestOptions {
+function parseUrlToOptions (url: string, https: boolean, type: 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'GET'): RequestOptions {
   const parsed = parseUrl(url)
 
   return {
     hostname: parsed.hostname,
     port: parsed.port ? parsed.port : https ? 443 : 80,
     path: parsed.path,
-    method: 'POST',
+    method: type,
     headers: {
       'Content-Type': 'application/json'
     }
   }
+}
+
+function request (options, https: boolean, callback?: Function) {
+  const cb = (res: IncomingMessage) => {
+    let data = ''
+
+    res.setEncoding('utf8')
+
+    res.on('data', chunk => {
+      data += chunk
+    })
+
+    res.on('end', () => {
+      return callback(data)
+    })
+  }
+
+  if (https) {
+    return httpsReq(options, cb)
+  }
+
+  return httpReq(options, cb)
 }
